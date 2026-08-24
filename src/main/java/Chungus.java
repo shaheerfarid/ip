b8f445c39d6bd622da9402f58782cf3c8c513cf3
shaheer-the-goat <shaheerfarid7@gmail.com>
2026-08-24 17:22:18 +0800

    Add Level-0: rename, greet, exit

---
+++ src/main/java/Chungus.java
@@ -0,0 +1,16 @@
+public class Chungus {
+    public static void main(String[] args) {
+        String banner = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("____________________________________________________________");
+        System.out.println(banner);
+        System.out.println("Hello, I am Chungus.\nWhat can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you back!");
+        
+
+    }
+}
