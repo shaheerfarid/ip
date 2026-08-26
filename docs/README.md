d5160e81f0d95288c790df9b30bc6b7b5221b134
shaheer-the-goat <shaheerfarid7@gmail.com>
2026-08-27 00:01:21 +0800

    adds the user inputs to the list and shows the whole list when the user types list

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Shaheer User Guide
+# Chungus User Guide
 
 // Update the title above to match the actual product name
 
--- src/main/java/Shaheer.java
+++ src/main/java/Shaheer.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Shaheer {
     public static void main(String[] args) {
@@ -7,22 +9,32 @@ public class Shaheer {
             + "\\___ \\| |_| | / _ \\ | |_| |  _| |  _| | |_) |\n"
             + " ___) |  _  |/ ___ \\|  _  | |___| |___|  _ < \n"
             + "|____/|_| |_/_/   \\_\\_| |_|_____|_____|_| \\_\\\n";
-        System.out.println("____________________________________________________________");
+        String printDivider = "____________________________________________________________";
+        System.out.println(printDivider);
         System.out.println(banner);
         System.out.println("Hello, I am Shaheer.\nWhat can I do for you?");
-        System.out.println("____________________________________________________________");
-
+        System.out.println(printDivider);
+        List<String> tasks = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
-            System.out.println("____________________________________________________________");
+            System.out.println(printDivider);
             if (command.trim().equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you back!");
-                System.out.println("____________________________________________________________");
+                System.out.println(printDivider);
                 break;
             }
-            System.out.println(command);
-            System.out.println("____________________________________________________________");
+            if (command.trim().equalsIgnoreCase("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
+                }
+                System.out.println(printDivider);
+                continue;
+            }
+            // System.out.println(command);
+            tasks.add(command);
+            System.out.println("added: " + command);
+            System.out.println(printDivider);
         }
     }
 }
