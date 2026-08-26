a00cb40a3a41f7dbfc7d0b6ad368dbcf61de22d8
shaheer-the-goat <shaheerfarid7@gmail.com>
2026-08-26 23:34:18 +0800

    Add command echo and bye exit

--- src/main/java/Shaheer.java
+++ src/main/java/Shaheer.java
@@ -1,14 +1,28 @@
+import java.util.Scanner;
+
 public class Shaheer {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String banner = " ____  _   _    _    _   _ _____ _____ ____  \n"
+            + "/ ___|| | | |  / \\  | | | | ____| ____|  _ \\ \n"
+            + "\\___ \\| |_| | / _ \\ | |_| |  _| |  _| | |_) |\n"
+            + " ___) |  _  |/ ___ \\|  _  | |___| |___|  _ < \n"
+            + "|____/|_| |_/_/   \\_\\_| |_|_____|_____|_| \\_\\\n";
         System.out.println("____________________________________________________________");
         System.out.println(banner);
         System.out.println("Hello, I am Shaheer.\nWhat can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you back!");
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+            if (command.trim().equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you back!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println(command);
+            System.out.println("____________________________________________________________");
+        }
     }
 }
