011631f150a18d81697615adba1dbdb116d3598c
shaheer-the-goat <shaheerfarid7@gmail.com>
2026-08-26 11:44:48 +0800

    Rename chatbot to Shaheer

--- src/main/java/Shaheer.java
+++ src/main/java/Shaheer.java
@@ -1,16 +1,14 @@
-public class Chungus {
+public class Shaheer {
     public static void main(String[] args) {
         String banner = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
+                + "| | | | | | | |/ / _ \\ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("____________________________________________________________");
         System.out.println(banner);
-        System.out.println("Hello, I am Chungus.\nWhat can I do for you?");
+        System.out.println("Hello, I am Shaheer.\nWhat can I do for you?");
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you back!");
-        
-
     }
 }
